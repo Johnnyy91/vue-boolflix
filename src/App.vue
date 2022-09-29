e8835734424eb911b7d6b151d3fe9b9a3b3a1df8
Johnnyy91 <claudiosardilli@gmail.com>
2022-09-29 12:16:56 +0200

    millestone 1-2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="app">
     <HeaderComponent @textToSearch='inputAssign'/>
+    <MainComponent :movies='ArrayMovies' :serieTv='ArraySerieTv'/>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
 import HeaderComponent from '@/components/HeaderComponent.vue'
+import MainComponent from './components/MainComponent.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderComponent,
+    MainComponent
 },
 data(){
   return{
     resultInput : '',
+    ArrayMovies : [],
+    ArraySerieTv : [],
   }
 },
   created(){
@@ -26,11 +31,19 @@ data(){
       this.resultInput = insertText
 
       axios
-        .get(`https://api.themoviedb.org/3/search/movie?api_key=4f84838e7f72cd5486a3c0f5685cf054&language=en-US&query=${this.resultInput}&page=1&include_adult=false`)
+        .get(`https://api.themoviedb.org/3/search/movie?api_key=4f84838e7f72cd5486a3c0f5685cf054&language=it-IT&query=${this.resultInput}&page=1&include_adult=false`)
         .then((response) => {
           console.log(response)
           console.log(response.data.results)
+          this.ArrayMovies = response.data.results
         })
+        axios
+          .get(`https://api.themoviedb.org/3/search/tv?api_key=4f84838e7f72cd5486a3c0f5685cf054&language=it-IT&query=${this.resultInput}&include_adult=false`)
+          .then ((response) =>{
+            console.log('serie tv',response)
+            console.log('serie tv',response.data.results)
+            this.ArraySerieTv = response.data.results
+          })
     }
   }
 }
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+    <h2 v-if="movies.length > 0">MOVIES</h2>
+    <div v-for="movie in movies" :key="movie.id">
+    <h4>TITLE {{movie.title}}</h4>
+    <h5>ORIGINAL TITLE {{movie.original_title}}</h5>
+    <p class="flag-img">
+      LANGUAGE <img :src="viewFlag(movie.original_language)" alt="">
+    </p>
+    <div>VOTE {{movie.vote_average}}</div>
+    </div>
+
+    <h2 v-if="serieTv.length > 0">SERIES TV</h2>
+    <div v-for="serie in serieTv" :key="serie.id">
+      <h4>TITLE {{serie.name}}</h4>
+      <h5>ORIGINAL TITLE {{serie.original_name}}</h5>
+      <p class="flag-img">
+        LANGUAGE <img :src="viewFlag(serie.original_language)" alt="">
+      </p>
+      <div>VOTE {{serie.vote_average}}</div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+name : 'MainComponent',
+data(){
+  return{
+  }
+},
+props : {
+  movies : Array,
+  serieTv : Array,
+},
+methods : {
+  viewFlag(flag){
+    if(flag === 'en' || flag ==='uk'){
+      flag = 'gb'
+    }else if(flag === 'ja'){
+      flag = 'jp'
+    }
+    return `https://flagicons.lipis.dev/flags/4x3/${flag}.svg`
+    
+  }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+.flag-img{
+  width: 20px;
+}
+</style>
